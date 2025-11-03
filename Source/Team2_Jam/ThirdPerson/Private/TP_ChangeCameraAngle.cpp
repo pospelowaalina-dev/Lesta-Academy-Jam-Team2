85dfc0b87a8348eaf54e76a1307c60644f75f723
Alina Pospelova <pospelowa.alina@gmail.com>
2025-11-03 03:07:57 +0300

    Camera rotation

---
+++ Source/Team2_Jam/ThirdPerson/Private/TP_ChangeCameraAngle.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "../Public/TP_ChangeCameraAngle.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Team2_Jam/ThirdPerson/Public/TP_ThirdPersonCharacter.h"
+
+ATP_ChangeCameraAngle::ATP_ChangeCameraAngle()
+{
+	CameraRotator = FRotator(0, 0, 0);
+	
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	RootComponent = Collider;
+
+	Collider->SetBoxExtent(FVector(100.0f, 100.0f, 1000.0f)); // Set desired size
+	Collider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Collider->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	Collider->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap); // Or ECC_WorldDynamic, etc.
+	Collider->SetGenerateOverlapEvents(true);
+}
+
+void ATP_ChangeCameraAngle::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Collider->OnComponentBeginOverlap.AddDynamic(this, &ATP_ChangeCameraAngle::OnOverlapBegin);
+}
+
+void ATP_ChangeCameraAngle::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this))
+	{
+		if (TObjectPtr<ATP_ThirdPersonCharacter> Character = Cast<ATP_ThirdPersonCharacter>(OtherActor))
+		{
+			Character->GetCameraBoom()->SetRelativeRotation(CameraRotator);
+		}
+	}
+}
---
+++ Source/Team2_Jam/ThirdPerson/Public/TP_ChangeCameraAngle.h
@@ -0,0 +1,28 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "TP_ChangeCameraAngle.generated.h"
+
+UCLASS()
+class ATP_ChangeCameraAngle : public AActor
+{
+	GENERATED_BODY()
+
+	ATP_ChangeCameraAngle();
+	
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Components, meta = (AllowPrivateAccess = "true"))
+	UBoxComponent *Collider = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	FRotator CameraRotator;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
